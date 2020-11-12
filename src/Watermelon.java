22d364c6b00b09d3c85ce550fc0fa7d739925934
Joseph <yusif.mammadli.98@gmail.com>
2020-11-12 11:35:54 -0500

    write ur code here

--- src/Watermelon.java
+++ src/Watermelon.java
@@ -1,6 +1,6 @@
 public class Watermelon {
     public static void main(String[] args) {
-        // write your code here
+        // try write your code here
         // are we there yet or not
         // what is up with you guys
     }
