1d4149456aab7b0f9d0cf3813ce54bf8131d86a7
Joseph <yusif.mammadli.98@gmail.com>
2020-11-12 10:45:14 -0500

    working on nice messages 1

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 public class main {
+
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello Git Last Day");
+        // B21 is excited!!
     }
 }
