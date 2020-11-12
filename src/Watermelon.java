37ec294ab72d79198b40d59ad2a802e8591bc782
Joseph <yusif.mammadli.98@gmail.com>
2020-11-12 11:33:37 -0500

    are we there yet or not

--- src/Watermelon.java
+++ src/Watermelon.java
@@ -1,7 +1,7 @@
 public class Watermelon {
     public static void main(String[] args) {
         // write your code here
-        // are we there yet
+        // are we there yet or not
         // what is up with you guys
     }
 }
